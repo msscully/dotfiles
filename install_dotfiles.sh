2275df24e4723bd19fa0da15f897d1023762e975
Mark Scully <mark.s.scully@gmail.com>
2013-11-19 13:14:07 -0600

    Add git-flow-completion.

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -4,3 +4,5 @@ cat $HOME/.dotfiles/bashrc >> $HOME/.bashrc
 cat $HOME/.dotfiles/bash_profile >> $HOME/.bash_profile
 ln -s $HOME/.dotfiles/vim $HOME/.vim
 ln -s $HOME/.dotfiles/vimrc $HOME/.vimrc
+mkdir $HOME/bash_completion.d
+ln -s git-flow-completion/git-flow-completion.bash $HOME/bash_completion.d/git-flow-completion.bash
