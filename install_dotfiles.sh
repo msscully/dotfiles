a86962ac8580e253d5da4cee7c373c61eda037c3
Mark Scully <mark.s.scully@gmail.com>
2013-12-07 10:14:12 -0600

    Added git submodule init & update to install script.

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+CURRENT_DIR=`pwd`
+cd ~/.dotfiles
+git submodule init # idempotent
+git submodule update
+cd $CURRENT_DIR
 cat $HOME/.dotfiles/bashrc >> $HOME/.bashrc
 cat $HOME/.dotfiles/bash_profile >> $HOME/.bash_profile
 ln -s $HOME/.dotfiles/vim $HOME/.vim
