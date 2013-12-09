b4d5485c2dc57d6d13a6eab05059b9fb9933d654
Mark Scully <mark.s.scully@gmail.com>
2013-12-09 10:33:57 -0600

    Added global gitignore for vim & osx.

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -10,4 +10,9 @@ cat $HOME/.dotfiles/bash_profile >> $HOME/.bash_profile
 ln -s $HOME/.dotfiles/vim $HOME/.vim
 ln -s $HOME/.dotfiles/vimrc $HOME/.vimrc
 mkdir $HOME/bash_completion.d
-ln -s git-flow-completion/git-flow-completion.bash $HOME/bash_completion.d/git-flow-completion.bash
+ln -s $HOME/.dotfiles/git-flow-completion/git-flow-completion.bash $HOME/bash_completion.d/git-flow-completion.bash
+
+# Setup a global .gitignore
+git config --global core.excludesfile '~/.gitignore'
+
+ln -s $HOME/.dotfiles/gitignore $HOME/.gitignore
