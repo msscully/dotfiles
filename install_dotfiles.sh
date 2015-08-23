da680e8b7a706bdd46d088ffbef35ea48546031f
Mark Scully <mark.s.scully@gmail.com>
2015-08-23 16:23:39 -0700

    Link inputrc in install script

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -12,6 +12,8 @@ ln -s $HOME/.dotfiles/vimrc $HOME/.vimrc
 mkdir $HOME/bash_completion.d
 ln -s $HOME/.dotfiles/git-flow-completion/git-flow-completion.bash $HOME/bash_completion.d/git-flow-completion.bash
 
+ln -s $HOME/.dotfiles/inputrc $HOME/.inputrc
+
 ln -s $HOME/.dotfiles/gitignore $HOME/.gitignore
 ln -s $HOME/.dotfiles/gitconfig $HOME/.gitconfig
 
